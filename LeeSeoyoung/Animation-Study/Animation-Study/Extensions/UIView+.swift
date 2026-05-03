4ac39e36209f223feaa89ece73e82cfa3bf23775
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-04 02:27:18 +0900

    feat: 부르르 버튼

--- LeeSeoyoung/Animation-Study/Animation-Study/Extensions/UIView+.swift
+++ LeeSeoyoung/Animation-Study/Animation-Study/Extensions/UIView+.swift
@@ -39,4 +39,16 @@ extension UIView {
             toastMessageView.removeFromSuperview()
         }
     }
+    
+    func shakeButton() {
+        self.transform = CGAffineTransform(translationX: 0, y: 20)
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0,
+            usingSpringWithDamping: 0.2,
+            initialSpringVelocity: 1
+        ) {
+            self.transform = .identity
+        }
+    }
 }
--- LeeSeoyoung/Animation-Study/Animation-Study/week1/BurrBurrViewController.swift
+++ LeeSeoyoung/Animation-Study/Animation-Study/week1/BurrBurrViewController.swift
@@ -7,10 +7,54 @@
 
 import UIKit
 
+import Then
+import SnapKit
+
 class BurrBurrViewController: UIViewController {
+    
+    private let jokebear = UIImageView().then {
+        $0.image = .jokebear
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let button = UIButton().then {
+        $0.setTitle("부르르 부르르 ...", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .deepBlue
+        $0.layer.cornerRadius = 10
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUI()
+        setLayout()
+        setAddTarget()
+    }
+    
+    private func setUI() {
+        view.backgroundColor = .white
+        [jokebear, button].forEach { self.view.addSubview($0) }
+    }
+    
+    private func setLayout() {
+        jokebear.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(100)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(400)
+            $0.height.equalTo(400)
+        }
+        button.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(100)
+            $0.horizontalEdges.equalToSuperview().inset(40)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    private func setAddTarget() {
+        button.addTarget(self, action: #selector(buttonDidTap), for: .touchUpInside)
+    }
+    
+    @objc private func buttonDidTap() {
+        button.shakeButton()
     }
 }
