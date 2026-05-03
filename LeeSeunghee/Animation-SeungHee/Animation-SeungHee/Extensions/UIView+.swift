250f74602f103eedddaa37995f18a5da599568f9
seunghee <daalgwabyul@naver.com>
2026-05-03 23:53:55 +0900

    feat: 토스트 메세지 띄우기

--- LeeSeunghee/Animation-SeungHee/Animation-SeungHee/Extensions/UIView+.swift
+++ LeeSeunghee/Animation-SeungHee/Animation-SeungHee/Extensions/UIView+.swift
@@ -6,10 +6,31 @@
 //
 
 import UIKit
+import SnapKit
 
 extension UIView {
     
     func addSubviews(_ views: UIView...) {
         views.forEach { self.addSubview($0)}
+    }
+    
+    
+    func showToast(_ title: String) {
+        let toastMessageView = ToastMessageView()
+        toastMessageView.setText(title)
+        self.addSubview(toastMessageView)
+        
+        toastMessageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalTo(35)
+        }
+        
+        toastMessageView.layer.cornerRadius = 8
+        
+        UIView.animate(withDuration: 1.5) {
+            toastMessageView.alpha = 0
+        } completion: { _ in
+            toastMessageView.removeFromSuperview()
+        }
     }
 }
