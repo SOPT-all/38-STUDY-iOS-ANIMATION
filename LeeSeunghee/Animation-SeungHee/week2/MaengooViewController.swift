809919e9fb0790bcf3f48f1332254eb77c150df6
seunghee <daalgwabyul@naver.com>
2026-05-25 20:52:53 +0900

    feat: 맹구야 움직여

--- LeeSeunghee/Animation-SeungHee/week2/MaengooViewController.swift
+++ LeeSeunghee/Animation-SeungHee/week2/MaengooViewController.swift
@@ -12,9 +12,57 @@ import Then
 
 class MaengooViewController: UIViewController {
     
+    //MARK: - UI Property
+    
     private let maengoo = UIImageView().then {
         $0.image = .maengoo
         $0.isUserInteractionEnabled = true
     }
     
+    //MARK: - viewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setUI()
+        setLayout()
+        setAction()
+    }
+    
+    //MARK: - UI Setting
+    
+   private func setUI() {
+        view.addSubviews(maengoo)
+    }
+    
+    private func setLayout() {
+        maengoo.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(150)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    private func setAction() {
+        maengoo.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+        maengoo.addGestureRecognizer(UIPanGestureRecognizer(target: self,action: #selector(handlePan)))
+    }
+    
+    //MARK: - functions
+    
+    @objc private func handleTap() {
+        UIView.animate(withDuration: 0.5) {
+            self.maengoo.transform = self.maengoo.transform.rotated(by: .pi)
+        }
+    }
+    
+    @objc
+    private func handlePan(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        
+        if let target = gesture.view {
+            target.center = CGPoint(x: target.center.x + translation.x, y: target.center.y + translation.y)
+        }
+        gesture.setTranslation(.zero, in: view)
+    }
 }
